ee1d2b52dc0301d5f63676e196558167c5a0e7b7
Augustana Library <wassail@ualberta.ca>
2015-04-20 11:21:11 -0600

    Updating SQL to property build  table

--- install.sql
+++ install.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.10
+-- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: wassail
 -- ------------------------------------------------------
--- Server version	5.0.26-log
+-- Server version	5.5.41-0+wheezy1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,8 +20,10 @@
 --
 
 DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `instance_id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(128) NOT NULL,
@@ -30,152 +32,231 @@ CREATE TABLE `account` (
   `right_write_unconditional` tinyint(1) NOT NULL,
   `right_report` tinyint(1) NOT NULL,
   `right_help` tinyint(1) NOT NULL,
-  `right_account` tinyint(1) NOT NULL default '0',
-  PRIMARY KEY  (`id`),
+  `right_account` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
   KEY `id` (`id`),
   KEY `instance_id` (`instance_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=161 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=458 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `answer`
 --
 
 DROP TABLE IF EXISTS `answer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `answer` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `instance_id` int(11) NOT NULL,
-  `question_id` int(11) NOT NULL default '0',
+  `question_id` int(11) NOT NULL DEFAULT '0',
   `text` text NOT NULL,
-  `position` smallint(6) NOT NULL default '1',
-  `asked` tinyint(1) default '0',
-  `date_added` int(11) NOT NULL default '0',
-  `correct` tinyint(1) default '0',
-  PRIMARY KEY  (`id`),
+  `position` smallint(6) NOT NULL DEFAULT '1',
+  `asked` tinyint(1) DEFAULT '0',
+  `date_added` int(11) NOT NULL DEFAULT '0',
+  `correct` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
   KEY `id` (`id`),
   KEY `instance_id` (`instance_id`),
   KEY `question_id` (`question_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9264 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=15475 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `category`
 --
 
 DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `category` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `text` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `text` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `course`
 --
 
 DROP TABLE IF EXISTS `course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `course` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `instance_id` int(11) NOT NULL,
-  `number` varchar(255) default NULL,
-  `name` varchar(255) default NULL,
-  `instructor` varchar(255) default NULL,
-  `date_added` int(11) default NULL,
-  `asked` tinyint(1) NOT NULL default '0',
-  PRIMARY KEY  (`id`),
+  `number` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `instructor` varchar(255) DEFAULT NULL,
+  `date_added` int(11) DEFAULT NULL,
+  `asked` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
   KEY `id` (`id`),
   KEY `instance_id` (`instance_id`),
   KEY `number` (`number`),
   KEY `name` (`name`),
   KEY `asked` (`asked`)
-) ENGINE=MyISAM AUTO_INCREMENT=500 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1120 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `help`
 --
 
 DROP TABLE IF EXISTS `help`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `help` (
   `page` varchar(255) NOT NULL,
   `content` longtext,
-  PRIMARY KEY  (`page`)
+  PRIMARY KEY (`page`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `import_answers`
+--
+
+DROP TABLE IF EXISTS `import_answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `import_answers` (
+  `old_id` int(11) DEFAULT NULL,
+  `new_id` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `import_courses`
+--
+
+DROP TABLE IF EXISTS `import_courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `import_courses` (
+  `old_id` varchar(50) DEFAULT NULL,
+  `new_id` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `import_questions`
+--
+
+DROP TABLE IF EXISTS `import_questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `import_questions` (
+  `old_id` int(11) DEFAULT NULL,
+  `new_id` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `import_templates`
+--
+
+DROP TABLE IF EXISTS `import_templates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `import_templates` (
+  `old_id` int(11) DEFAULT NULL,
+  `new_id` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `instance`
 --
 
 DROP TABLE IF EXISTS `instance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `instance` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) default NULL,
-  PRIMARY KEY  (`id`),
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=72 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `question`
 --
 
 DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `question` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `instance_id` int(11) NOT NULL,
   `text` text NOT NULL,
   `plaintext` text NOT NULL,
   `tags` text,
-  `type` enum('multiple','single','qualitative') NOT NULL default 'single',
-  `qualitative_type` enum('long','short') default 'long',
-  `date_added` int(11) NOT NULL default '0',
-  `asked` tinyint(1) NOT NULL default '0',
-  `opt_out` varchar(255) NOT NULL default 'I prefer not to answer/not applicable',
-  PRIMARY KEY  (`id`),
+  `type` enum('multiple','single','qualitative') NOT NULL DEFAULT 'single',
+  `qualitative_type` enum('long','short') DEFAULT 'long',
+  `date_added` int(11) NOT NULL DEFAULT '0',
+  `asked` tinyint(1) NOT NULL DEFAULT '0',
+  `opt_out` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `instance_id` (`instance_id`),
   KEY `date_added` (`date_added`),
   FULLTEXT KEY `plaintext` (`plaintext`)
-) ENGINE=MyISAM AUTO_INCREMENT=2333 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4065 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `question_category`
 --
 
 DROP TABLE IF EXISTS `question_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `question_category` (
-  `question_id` int(11) default NULL,
-  `category_id` int(11) default NULL,
+  `question_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
   KEY `ids` (`question_id`,`category_id`),
   KEY `category_id` (`category_id`),
   KEY `question_id` (`question_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `question_template`
 --
 
 DROP TABLE IF EXISTS `question_template`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `question_template` (
   `question_id` int(11) NOT NULL,
   `template_id` int(11) NOT NULL,
   `position` int(11) NOT NULL,
-  PRIMARY KEY  (`question_id`,`template_id`)
+  PRIMARY KEY (`question_id`,`template_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `response`
 --
 
 DROP TABLE IF EXISTS `response`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `response` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `instance_id` int(11) NOT NULL,
   `response_id` int(11) NOT NULL,
   `number` int(11) NOT NULL,
   `template_id` int(11) NOT NULL,
   `course_id` int(11) NOT NULL,
   `term` int(11) NOT NULL,
-  `questionnaire_type` int(11) default NULL,
+  `questionnaire_type` int(11) DEFAULT NULL,
   `school_year` varchar(255) NOT NULL,
   `question_id` int(11) NOT NULL,
   `answer_id` int(11) NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   KEY `response_id` (`response_id`),
   KEY `number` (`number`),
   KEY `template_id` (`template_id`),
@@ -183,87 +264,108 @@ CREATE TABLE `response` (
   KEY `term` (`term`),
   KEY `questionnaire_type` (`questionnaire_type`),
   KEY `school_year` (`school_year`)
-) ENGINE=MyISAM AUTO_INCREMENT=540016 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=706381 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `response_id`
 --
 
 DROP TABLE IF EXISTS `response_id`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `response_id` (
-  `id` int(11) NOT NULL auto_increment,
-  `filename` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=177166 DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filenames` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=193880 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `response_qualitative`
 --
 
 DROP TABLE IF EXISTS `response_qualitative`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `response_qualitative` (
   `question_response_id` int(11) NOT NULL,
   `text` text NOT NULL,
-  PRIMARY KEY  (`question_response_id`)
+  PRIMARY KEY (`question_response_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `session`
 --
 
 DROP TABLE IF EXISTS `session`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `session` (
-  `sid` varchar(128) default NULL,
-  `time` int(11) default NULL
+  `sid` varchar(128) DEFAULT NULL,
+  `time` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `template`
 --
 
 DROP TABLE IF EXISTS `template`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `template` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `instance_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `date_added` int(11) NOT NULL,
-  `asked` tinyint(1) NOT NULL default '0',
-  PRIMARY KEY  (`id`),
+  `asked` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
   UNIQUE KEY `instance_id` (`instance_id`,`name`)
-) ENGINE=MyISAM AUTO_INCREMENT=1535 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3057 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `web_form`
 --
 
 DROP TABLE IF EXISTS `web_form`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `web_form` (
-  `id` int(11) NOT NULL auto_increment,
-  `instance_id` int(11) default NULL,
-  `template_id` int(11) default NULL,
-  `course_id` int(11) default NULL,
-  `term` int(11) default NULL,
-  `questionnaire_type` int(11) default NULL,
-  `school_year` varchar(255) default NULL,
-  `expiry_date` int(11) default NULL,
-  `number` int(11) default '0',
-  `public` tinyint(1) default '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `instance_id` int(11) DEFAULT NULL,
+  `template_id` int(11) DEFAULT NULL,
+  `course_id` int(11) DEFAULT NULL,
+  `term` int(11) DEFAULT NULL,
+  `questionnaire_type` int(11) DEFAULT NULL,
+  `school_year` varchar(255) DEFAULT NULL,
+  `expiry_date` int(11) DEFAULT NULL,
+  `number` int(11) DEFAULT '0',
+  `public` tinyint(1) DEFAULT '0',
   `intro` text,
   `file_request` text,
-  `name` varchar(255) default NULL,
-  `email` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=609 DEFAULT CHARSET=latin1;
+  `file_count` tinyint(4) NOT NULL DEFAULT '1',
+  `name` varchar(255) DEFAULT NULL,
+  `confirmation` text,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1844 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `web_form_account`
 --
 
 DROP TABLE IF EXISTS `web_form_account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `web_form_account` (
-  `form_id` int(11) default NULL,
-  `password` varchar(128) default NULL
+  `form_id` int(11) DEFAULT NULL,
+  `password` varchar(128) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -274,4 +376,4 @@ CREATE TABLE `web_form_account` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-08-12 19:04:31
+-- Dump completed on 2015-04-20 11:19:37
